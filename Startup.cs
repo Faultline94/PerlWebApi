163dab783a678744bffb2716c563d392dcfcc44b
Faultline94 <faultline313@gmail.com>
2022-03-28 11:41:19 +0200

    Added Startup

---
+++ Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+using DbCRUDReposLib;
+using DbContextLib;
+
+namespace PerlWebApi
+{
+    public class Startup
+    {
+
+    }
+}
