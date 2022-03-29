9dd80b75ad064bd4c99f26dc7bf20e92b04572b4
Faultline94 <faultline313@gmail.com>
2022-03-29 12:03:30 +0200

    GetNecklace() fix

--- Controllers/NecklacesController.cs
+++ Controllers/NecklacesController.cs
@@ -21,7 +21,30 @@ namespace PerlWebApi.Controllers
 
         public async Task<IEnumerable<Necklace>> GetNecklaces()
         {
+            var neck = await _repo.ReadAllAsync();
+            return neck;
+        }
 
+        [HttpGet("{necklaceId}", Name = nameof(GetNecklace))]
+        [ProducesResponseType(200, Type = typeof(Necklace))]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        public async Task<IActionResult> GetNecklace(string necklaceId)
+        {
+            if (int.TryParse(necklaceId, out int neckVar))
+            {
+                return BadRequest("Guid format error");
+            }
+            Necklace neck = await _repo.ReadAsync(neckVar);
+            if (neck != null)
+            {
+                //cust is returned in the body
+                return Ok(neck);
+            }
+            else
+            {
+                return NotFound();
+            }
         }
     }
 }
